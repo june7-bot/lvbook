68d4e1690b093d36dde61813420b05158f48a766
--global <--global>
2020-12-06 22:21:00 +0900

    '1206'

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -12,4 +12,42 @@ class ArticlesController extends Controller
 
     return view('articles.index', compact('articles'));
     }
+
+    public function create(){
+        return view('articles.create');
+    }
+
+    public function store(Request $request)
+    {
+        $rules = [
+            'title' => ['required'],
+            'content' => ['required',  'min:10'],
+        ];
+
+        $messages = [
+            'title.required' => '제목은 필수 입력 항목입니다',
+            'content.required' => '본문은 필수 입력 항목입니다.',
+            'content.min' => '본문은 최소 :min 글자 이상 필요합니다',
+        ];
+
+
+        $validator = \Validator::make($request->all(), $rules , $messages);
+
+        if($validator -> fails() ){
+            return back()->withErrors($validator)
+                        ->withInput();
+        }
+
+        $article = \App\User::find(1)->articles()
+                                    ->create($request->all());
+
+        if(! $article) {
+            return back()->with('flash_message', '글이 저장되지않았습니다')
+                            ->withInput();
+        }
+
+        return redirect(route('articles.index'))
+                ->with('flash_message', '작성하신 글이 저장되었습니다');
+
+    }
 }
